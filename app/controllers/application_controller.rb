a9b816d218f72ae80bba617c0c609318ad3e49f8
woeishyonglim <woeishyonglim1989@gmail.com>
2015-05-22 09:55:35 +0800

    add authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['s3_username'] && password == ENV['s3_password']
+    end
+  end
 end
